eaf6a73aa3c5d0ab09c6a1aa73e0f2cbe704b718
saad277 <saadsheikh27@gmail.com>
2021-06-07 00:48:57 +0500

    libs

---
+++ src/App/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>saaass222sa</Text>
+    </View>
+  );
+};
+
+export default App;
